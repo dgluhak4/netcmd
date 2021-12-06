32293cc5fa362e789e5d52175047e6edacac1da4
dgluhak4 <31708249+dgluhak4@users.noreply.github.com>
2021-12-06 23:15:32 +0100

    ispravke-211206-7

--- netcmd.py
+++ netcmd.py
@@ -404,9 +404,11 @@ Main function that deploys list of commands to a list of devices and prints/pars
                     #print(device["output"])
                 device["output"].clear()       
             except OSError:
-                continue_error("Username/password error or reachability error. Skiping device {}, going to next.".format(device['host']))          
+                continue_error("Username/password error or reachability error. Skiping device {}, going to next.".format(device['host']))   
+            except NetmikoTimeoutException:
+                continue_error("TCP Connection to device {} failed".format(device['']))       
             except:
-                continue_error("Neka greska u komunikaciji s {}".format(device['host']))          
+                continue_error("Neka greska u komunikaciji s {}".format(device['device']['host']))          
         sys_params['timestamps'].append(time.time())
         stats_output(citer,count_ops,sys_params,True)
     #while loop control mechanism
